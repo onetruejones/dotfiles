9454afc19facf7c5c3d3e73c787d6920fbca5aaa
Matt Hogan-Jones <onetruejones@gmail.com>
2021-12-29 11:13:29 +0000

    First commit of Neovim config

---
+++ nvim/.config/nvim/lua/_options.lua
@@ -0,0 +1,36 @@
+--Remap space as leader key
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.border_style = "rounded"
+
+--Remap for dealing with word wrap
+vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+--Make line numbers default
+vim.wo.number = true
+
+--Set scroll offset
+vim.o.scrolloff = 8
+
+--Enable mouse mode
+vim.o.mouse = 'a'
+--Set highlight on search
+vim.o.hlsearch = false
+--Enable break indent
+vim.o.breakindent = true
+
+--Save undo history
+vim.opt.undofile = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
