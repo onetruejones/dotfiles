ba199179581dd6f8adbe2b8cf9ff11b23868264d
Matt Hogan-Jones <onetruejones@gmail.com>
2024-07-04 20:05:14 +0100

    testing

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("_options")
 
--- Install packer
+-- Install packer - making a change
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
